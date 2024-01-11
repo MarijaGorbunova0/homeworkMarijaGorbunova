b97376328a2288b24f12ebfab4632fcf0c779882
opilane <opilane@HP-CZC2349HTD>
2024-01-11 12:12:45 +0200

    Add project files.

---
+++ homeworkMarijaGorbunova/homeworkMarijaGorbunova.py
@@ -0,0 +1,90 @@
+﻿#1
+#print("Tere maailm")
+#nimi=input("Mis on sinu nimi?") #comment
+#vanus=int(input("Kui vana sa oled?")) #float()->2.5
+#print("Tere tulemas! "+nimi+". Sa oled"+str(vanus)+" aastat vana")
+#print("Tere tulemast! ",nimi,". Sa oled",vanus,"aasatat vana")
+#print("Tere tulemast! {0}.Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja vanus",vanus,"tüüp on",type(vanus))
+
+#2
+#vanus= 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#Kas käib kollis = True
+#print("Muutuja vanus",eesnimi,"tüüp on",type(eesnimi))
+#print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
+#print("Muutuja vanus",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
+#print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
+
+#3
+#from random import *
+#konfeti=randint(10,100)
+#print("konfeti",konfeti)
+#vzal=int(input("skolko vzal"))
+#konfeti-=vzal
+#print("siin on","konfet")
+#print()
+#user = int(input("длинна окружности дерева - "))
+#kokku = user/3.14
+#print("диаметр окружности равен:", kokku)
+
+#5
+#try:
+#    N = int(input("первое число"))
+#    M = int(input("первое второе число"))
+#    kokku = N*M
+#    print("диагонали прямоугольного участка земли равен", kokku)
+#except :
+#    print("viga andmetüübiga")
+    
+#6
+#from string import printable
+
+
+#try:
+#    aeg = float(input("Сколько часов ушло на поездку?"))
+#    tee = float(input("Сколько километров ты проехал?"))
+#    kiirus= tee/aeg
+#    print(kiirus)
+#except:
+#    print("viga andmetüübiga")
+
+
+#try:
+#    numbers = (1,2,3,4,5)
+#    kokku = sum(numbers)/len(numbers)
+#    print(kokku)
+#except :
+#    print("viga andmetüübiga")
+
+
+#8
+#frog ="""
+#@..@
+#(----)
+#( __/ )
+#^^ "" ^^
+#"""
+
+#9
+#try:
+#    a = int(input("первое число"))
+#    b = int(input("второе число"))
+#    c = int(input("третье число"))
+#    kokku = a+b+c
+#    print("периметр треугольника:", kokku)
+#except :
+#    print("viga andmetüübiga")
+    
+
+#try:
+#    from random import *
+#    p = randint(1,5)
+#    hind = 12.90
+#    hind*= 1.1 #hind koos jatatega 
+#    osa= round(hind/p,2) #округляем 2 знака после запитой 
+#    print("Iga sõber maksab: ",osa)
+#except :
+#     print("viga andmetüübiga")
+
